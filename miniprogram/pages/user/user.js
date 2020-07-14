a86bbe49d311c90503e55f6278d1a78f482be5da
yif <3180101376@zju.edu.cn>
2020-07-14 16:05:41 +0800

    1.0.0上线

--- miniprogram/pages/user/user.js
+++ miniprogram/pages/user/user.js
@@ -1,20 +1,42 @@
-// pages/user/user.js
 Page({
 
 	/**
 	 * 页面的初始数据
 	 */
 	data: {
-
+		userInfo:null
 	},
 
 	onShow: function () {
 		this.getTabBar().init();
+		const userInfo=wx.getStorageSync('userinfo')
+		this.setData({userInfo})
 	},
 
 	toAbout(){
 		wx.navigateTo({
 			url: '/pages/about/about',
+		})
+	},
+
+	handleLogin(){
+		this.setData({show:true})
+	},
+
+	onClose() {
+    this.setData({ show: false });
+  },
+
+	getUserInfo(e){
+		console.log(e);
+		const {userInfo}=e.detail;
+		this.setData({userInfo})
+		wx.setStorageSync("userinfo", userInfo);
+	},
+
+	toUserInfo(){
+		wx.navigateTo({
+			url: '/pages/user_info/user_info',
 		})
 	}
 
